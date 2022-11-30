50e464b72161514b86618d1523c06c9f4b5a1147
Yanuar2301 <gideonsyanuar@gmail.com>
2022-11-30 21:09:04 +0800

    Revisi

--- db/aplikasipengelolaanlaundry.sql
+++ db/aplikasipengelolaanlaundry.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.0.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 03, 2020 at 08:54 AM
--- Server version: 10.3.16-MariaDB
--- PHP Version: 7.1.30
+-- Generation Time: Nov 30, 2022 at 02:08 PM
+-- Server version: 10.4.25-MariaDB
+-- PHP Version: 8.1.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -36,24 +35,27 @@ CREATE TABLE `detailtransaksi` (
   `keterangan` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `detailtransaksi`
---
-
-INSERT INTO `detailtransaksi` (`id`, `id_transaksi`, `id_paket`, `qty`, `keterangan`) VALUES
-('DT001', '321', '3213', 123, 'dsfsdfsfs'),
-('DT002', '123', '3213', 123, '321');
-
 -- --------------------------------------------------------
 
 --
--- Table structure for table `level`
+-- Table structure for table `login`
 --
 
-CREATE TABLE `level` (
-  `id_level` char(11) NOT NULL,
-  `nama_level` enum('ADMINISTATOR') NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE `login` (
+  `id` int(11) NOT NULL,
+  `username` varchar(25) NOT NULL,
+  `password` varchar(25) NOT NULL,
+  `nama` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `login`
+--
+
+INSERT INTO `login` (`id`, `username`, `password`, `nama`) VALUES
+(1, 'Admin', 'Admin123', ''),
+(2, 'yans', 'yans123', 'Yanuar Gideon'),
+(4, 'Intang', 'intan123', 'Intan Wulandari');
 
 -- --------------------------------------------------------
 
@@ -66,15 +68,17 @@ CREATE TABLE `member` (
   `nama` varchar(100) NOT NULL,
   `alamat` text NOT NULL,
   `jenis_kelamin` enum('L','P') NOT NULL,
-  `tlp` varchar(15) NOT NULL
+  `tlp` varchar(15) NOT NULL,
+  `id_login` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `member`
 --
 
-INSERT INTO `member` (`id`, `nama`, `alamat`, `jenis_kelamin`, `tlp`) VALUES
-('K002', 'aikk', 'asek', 'P', '66466');
+INSERT INTO `member` (`id`, `nama`, `alamat`, `jenis_kelamin`, `tlp`, `id_login`) VALUES
+('M001', 'grace ', 'Giri Mukti Strat 5 No 76 Rt.08', 'P', '08172361726', 2),
+('M002', 'intan', 'sepaku rt.05 No 1', 'P', '083422734234', 4);
 
 -- --------------------------------------------------------
 
@@ -94,52 +98,55 @@ CREATE TABLE `outlet` (
 --
 
 INSERT INTO `outlet` (`id`, `nama`, `alamat`, `tlp`) VALUES
-('O001', 'Naufal Vigar Gani', 'jalan raudah', '123'),
-('O002', 'Musyahidah', 'jalan raudah', '085247572105');
+('O002', 'Intan Laundry', 'Perjuangan 7', '0814123337211'),
+('O003', 'Yans Laundry', 'Perjuangan 5', '0805821554441');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `paket`
+-- Table structure for table `pekerja`
 --
 
-CREATE TABLE `paket` (
-  `id` char(11) NOT NULL,
-  `id_outlet` char(11) NOT NULL,
-  `jenis` enum('kiloan','selimut','bed_cover','kaos','lain') NOT NULL,
-  `nama_paket` varchar(100) NOT NULL,
-  `harga` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `paket`
---
-
-INSERT INTO `paket` (`id`, `id_outlet`, `jenis`, `nama_paket`, `harga`) VALUES
-('DP002', '321', 'bed_cover', 'abc', 2000000);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `user`
---
-
-CREATE TABLE `user` (
+CREATE TABLE `pekerja` (
   `id` char(11) NOT NULL,
   `nama` varchar(100) NOT NULL,
-  `username` varchar(30) NOT NULL,
-  `password` text NOT NULL,
   `id_outlet` char(11) NOT NULL,
   `role` enum('admin','kasir','owner') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `user`
+-- Dumping data for table `pekerja`
 --
 
-INSERT INTO `user` (`id`, `nama`, `username`, `password`, `id_outlet`, `role`) VALUES
-('U002', 'musyahidah', 'admin', '12313', 'O001', 'admin'),
-('U003', 'Naufal Vigar Gani', 'admin', '12313', 'O002', 'kasir');
+INSERT INTO `pekerja` (`id`, `nama`, `id_outlet`, `role`) VALUES
+('U001', 'Deo', 'O003', 'owner'),
+('U002', 'Wulandari', 'O002', 'kasir');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transaksi`
+--
+
+CREATE TABLE `transaksi` (
+  `id` int(11) NOT NULL,
+  `id_outlet` varchar(30) NOT NULL,
+  `kode_invoice` varchar(30) NOT NULL,
+  `id_member` varchar(30) NOT NULL,
+  `tgl_masuk` varchar(30) NOT NULL,
+  `batas_waktu` varchar(30) NOT NULL,
+  `tgl_bayar` varchar(30) NOT NULL,
+  `status` varchar(30) NOT NULL,
+  `dibayar` varchar(30) NOT NULL,
+  `id_login` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `transaksi`
+--
+
+INSERT INTO `transaksi` (`id`, `id_outlet`, `kode_invoice`, `id_member`, `tgl_masuk`, `batas_waktu`, `tgl_bayar`, `status`, `dibayar`, `id_login`) VALUES
+(9, 'O002', '1111', 'M001', '1111-11-11', '1111-11-15', '1111-11-15', 'selesai', 'dibayar', 2);
 
 --
 -- Indexes for dumped tables
@@ -152,10 +159,10 @@ ALTER TABLE `detailtransaksi`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `level`
+-- Indexes for table `login`
 --
-ALTER TABLE `level`
-  ADD PRIMARY KEY (`nama_level`);
+ALTER TABLE `login`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `member`
@@ -170,18 +177,33 @@ ALTER TABLE `outlet`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `paket`
+-- Indexes for table `pekerja`
 --
-ALTER TABLE `paket`
+ALTER TABLE `pekerja`
   ADD PRIMARY KEY (`id`),
   ADD KEY `id_outlet` (`id_outlet`);
 
 --
--- Indexes for table `user`
+-- Indexes for table `transaksi`
 --
-ALTER TABLE `user`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `id_outlet` (`id_outlet`);
+ALTER TABLE `transaksi`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `login`
+--
+ALTER TABLE `login`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
